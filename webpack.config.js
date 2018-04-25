d4aa858eef2659ebbbfa1a8d362f96592c0c051d
Dennis Timmers <dennis@timmersnet.nl>
2018-04-25 19:53:53 +0200

    clear workspace

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './src/index.ts',
-  
+
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
 
   mode: 'development',
-  
+
   devtool: 'inline-source-map',
 
   devServer: {
@@ -43,4 +43,4 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin()
   ]
-};
+}
