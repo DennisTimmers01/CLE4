f47dc9dc21d56f36c8b3b38660f2cc83880ed287
Dennis Timmers <dennis@timmersnet.nl>
2018-05-07 09:59:48 +0200

    remove tutorial code

--- src/js/game/game.ts
+++ src/js/game/game.ts
@@ -2,8 +2,6 @@ import 'pixi';
 import 'p2';
 import 'phaser';
 
-import Platform from './environment/platform';
-
 class Game {
   element: HTMLElement;
   game: Phaser.Game;
@@ -28,26 +26,9 @@ class Game {
     );
   }
 
-  preload() {
-    const { load } = this.game;
-    load.image('sky', './assets/sky.png');
-    load.image('ground', './assets/platform.png');
-    load.image('star', './assets/star.png');
-    load.spritesheet('dude', './assets/dude.png', 32, 48);
-  }
+  preload() {}
 
-  create() {
-    const { add, world } = this.game;
-    add.sprite(0, 0, 'sky');
-    add.sprite(0, 0, 'star');
-
-    this.platforms = this.game.add.group();
-    this.platforms.enableBody = true;
-
-    Platform.createGround(this.platforms, 0, world.height - 30);
-    Platform.createLedge(this.platforms, 400, 400);
-    Platform.createLedge(this.platforms, -150, 250);
-  }
+  create() {}
 
   update() {}
 }
