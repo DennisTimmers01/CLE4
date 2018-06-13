0fc1eb14f816e4af3db9af0d3874a288c86d8e1c
Dennis Timmers <dennis@timmersnet.nl>
2018-06-13 11:47:13 +0200

    letter pickup function and create letters

--- src/js/game/letter/Letter.ts
+++ src/js/game/letter/Letter.ts
@@ -1,3 +1,5 @@
+import { level1 } from '../config/levelConfig';
+
 class Letter {
   _game: Phaser.Game;
   _letters: Phaser.Group;
@@ -8,10 +10,20 @@ class Letter {
   constructor(game: Phaser.Game, player: any) {
     this._game = game;
     this._player = player;
+
     this._letters = this._game.add.group();
     this._letters.enableBody = true;
 
-    this._letterArray = ['o', 'p', 'e', 'n'];
+    this._generateLetters();
+  }
+
+  private _createLetter(x: number, y: number, name: string) {
+    this._singleLetter = this._letters.create(x, y, name);
+  }
+
+  private _generateLetters() {
+    const { letters } = level1;
+    letters.map(letter => this._createLetter(letter.x, letter.y, letter.name));
   }
 
   public killLetter(_player: object, _singleLetter: any) {
